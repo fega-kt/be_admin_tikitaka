8e7bb301505ab2389cd2dbcda8d472fa472f2cf0
Kim Thai <fega.kt@gmail.com>
2023-11-13 02:01:14 +0700

    config format data list

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ExceptionsFilter } from './core/filter/exceptions.filter';
 import { environments } from './environments/environments';
+import { TransformInterceptor } from './transform.interceptor';
 @Module({
   imports: [
     FeaturesModule,
@@ -25,6 +26,10 @@ import { environments } from './environments/environments';
     {
       provide: APP_FILTER,
       useClass: ExceptionsFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
     },
   ],
   controllers: [AppController],
