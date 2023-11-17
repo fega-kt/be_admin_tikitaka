336d6ce5ae2c3af3a4a68f8cb115b139cff53e55
Kim Thai <fega.kt@gmail.com>
2023-11-17 22:12:12 +0700

    xóa khi không dùng

--- src/features/user/service/user.service.ts
+++ src/features/user/service/user.service.ts
@@ -5,7 +5,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model, UpdateQuery } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
 import { ObjectId } from 'mongodb';
 import { User } from '../schema/user.schema';
 import { randomString } from '../../../shared/utils/random-string';
