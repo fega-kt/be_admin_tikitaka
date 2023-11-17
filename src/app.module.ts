7ce1951f028d18f5b3b166b0d180c3a02ad8abda
Kim Thai <fega.kt@gmail.com>
2023-11-17 22:12:50 +0700

    xóa khi không dùng

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,9 @@ import {
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { ExceptionsFilter } from './core/filter/exceptions.filter';
 import { environments } from './environments/environments';
-import { TransformInterceptor } from './transform.interceptor';
 import { LoggingMiddleware } from './logging.middleware';
 @Module({
   imports: [
